4964dd1be66d5d499f3257f94412007992a9ebcd
RAZ <rbrtzavala@gmail.com>
2017-05-12 10:03:36 -0600

    Added dummy IMAGES to pass to ImageDetail

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,11 +1,23 @@
 // Create Image List component
 import React from 'react';
 
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400/fc00fc/00ff99' },
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400/fc00fc/00ff99' },
+  { title: 'Mug', link: 'https://dummyimage.com/600x400/fc00fc/00ff99'}
+];
+
 // Create component
 const ImageList = () => {
+  const RenderedImages = IMAGES.map((image) => {
+    return <ImageDetail picture={image} />
+  });
+
   return (
     <ul>
-      <li>Image Detail</li>
+      {RenderedImages}
     </ul>
   );
 };
